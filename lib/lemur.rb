01913d367b8c33c75002e7ab339c8e524d0bf421
Alexey Gaziev <alex.gaziev@gmail.com>
2014-11-05 16:06:08 +0800

    Легче взять блять и переписать все нахуй.

--- lib/lemur.rb
+++ lib/lemur.rb
@@ -4,75 +4,110 @@ require 'digest/md5'
 require 'multi_json'
 
 module Lemur
-  class ApiError < StandardError
-    attr_reader :data
-
-    def error_code
-      @data['error_code']
-    end
-
-    def error_data
-      @data['error_data']
-    end
-
-    def error_msg
-      @data['error_msg']
-    end
-
-    def initialize(data)
-      @data = data
-      message = "#{error_code}: #{error_msg}"
-      super(message)
-    end
-  end
-
-  class ApiParsingError < StandardError
-    attr_reader :url
-    attr_reader :body
-
-    def initialize(responce)
-      @url = responce.env[:url].to_s
-      @body = responce.body
-      message = "Wrong JSON at #{url}: #{body}"
-      super(message)
-    end
-  end
-
   ODNOKLASSNIKI_API_URL = 'http://api.odnoklassniki.ru/fb.do'
   ODNOKLASSNIKI_NEW_TOKEN_URL = 'http://api.odnoklassniki.ru/oauth/token.do'
 
   class API
+    attr_reader :security_options, :request_options, :response, :connection
+    attr_accessor :verbose_output
 
-    def initialize(application_secret_key, application_key,  access_token, application_id = nil, refresh_token = nil)
-      start_faraday
+    def initialize(opts)
+      check_required_options(opts)
+      @verbose_output = opts[:verbose_response]
       @security_options = {
-        application_secret_key: application_secret_key,
-        application_key: application_key, access_token: access_token,
-        application_id: application_id, refresh_token: refresh_token
+        application_secret_key: opts[:application_secret_key],
+        application_key:        opts[:application_key],
+        access_token:           opts[:access_token],
+        application_id:         opts[:application_id],
+        refresh_token:          opts[:refresh_token]
       }
     end
-
-    attr_reader :security_options, :request_options, :response, :connection
 
     def get_new_token
       if @security_options[:application_id].blank? || @security_options[:refresh_token].blank?
         raise ArgumentError, 'wrong number of arguments'
       end
-      faraday_options = {
-        request: {:timeout=>150, :open_timeout=>150},
-        url: Lemur::ODNOKLASSNIKI_NEW_TOKEN_URL
-      }
-      conn = init_faraday(faraday_options)
+      conn = connection_for_endpoint(Lemur::ODNOKLASSNIKI_NEW_TOKEN_URL)
       new_token_response = conn.post do |req|
         req.params = {
-          refresh_token: @security_options[:refresh_token], grant_type: 'refresh_token',
-          client_id: @security_options[:application_id],
-          client_secret: @security_options[:application_secret_key]
+          client_id:     @security_options[:application_id],
+          client_secret: @security_options[:application_secret_key],
+          refresh_token: @security_options[:refresh_token],
+          grant_type:    'refresh_token'
         }
       end
       new_token_response = MultiJson.load(new_token_response.body)
       @security_options[:access_token] = new_token_response['access_token']
       new_token_response['access_token']
+    end
+
+    def get(request_params)
+      @response = get_request(request_params)
+      json_data = \
+        begin
+          MultiJson.load(response.body)
+        rescue MultiJson::DecodeError
+          # Sometimes invalid json can be returned by Odnoklassniki
+          fixed_response = \
+            response.body.gsub(/[^"]}/, '"}').gsub(/([^"}]),"([^}])/, '\1","\2')
+          MultiJson.load(fixed_response)
+        end
+      if json_data.is_a?(Hash) && json_data['error_code']
+        raise ApiError, json_data
+      end
+
+      json_data
+    rescue MultiJson::DecodeError
+      raise ApiParsingError, @response
+    end
+
+    private
+
+    def final_request_params(request_params, access_token)
+      signature = ok_signature(
+        request_params,
+        access_token,
+        security_options[:application_secret_key]
+      )
+
+      request_options = {
+        sig:             signature,
+        access_token:    security_options[:access_token],
+        application_key: security_options[:application_key]
+      }
+
+      request_params.merge(request_options)
+    end
+
+    def ok_signature(request_params, access_token)
+      sorted_params_string = ""
+      key = security_options[:application_secret_key]
+      request_params = Hash[request_params.sort]
+
+      request_params.each {|key, value| sorted_params_string += "#{key}=#{value}"}
+      secret_part = Digest::MD5.hexdigest("#{access_token}#{key}")
+      Digest::MD5.hexdigest("#{sorted_params_string}#{secret_part}")
+    end
+
+    def start_faraday
+      @connection = init_faraday(faraday_options)
+    end
+
+    def connection_for_endpoint(url)
+      @connection ||= {}
+      @connection[url] ||= begin
+        faraday_options = {
+          request: {:timeout=>150, :open_timeout=>150},
+          url: url
+        }
+
+        Faraday.new(faraday_options) do |faraday|
+          faraday.headers['Content-Type'] = 'application/json'
+          faraday.request  :url_encoded
+          faraday.response(:logger) if verbose_output
+          faraday.adapter  Faraday.default_adapter
+        end
+      end
     end
 
     def get_request(request_params)
@@ -83,58 +118,12 @@ module Lemur
       end
     end
 
-    def get(request_params)
-      @response = get_request(request_params)
-      json_data = nil
-      begin
-        json_data = MultiJson.load(response.body)
-      rescue MultiJson::DecodeError
-        json_data = MultiJson.load(response.body.gsub(/[^"]}/, '"}').gsub(/([^"}]),"([^}])/, '\1","\2')) #Есть случаи отдачи кривого JSON от одноклассников
-      end
-      if json_data.is_a? Hash
-        if json_data['error_code']
-          raise ApiError, json_data
-        end
-      end
-      json_data
-    rescue MultiJson::DecodeError
-      raise ApiParsingError, @response
-    end
-
-    private
-
-    def final_request_params(request_params, access_token)
-      request_params.merge(sig: odnoklassniki_signature(request_params, access_token, security_options[:application_secret_key]),
-        access_token: security_options[:access_token],
-        application_key: security_options[:application_key])
-    end
-
-    def odnoklassniki_signature(request_params, access_token, application_secret_key)
-      sorted_params_string = ""
-      request_params = Hash[request_params.sort]
-      request_params.each {|key, value| sorted_params_string += "#{key}=#{value}"}
-      secret_part = Digest::MD5.hexdigest("#{access_token}#{application_secret_key}")
-      Digest::MD5.hexdigest("#{sorted_params_string}#{secret_part}")
-    end
-
-    def start_faraday
-      faraday_options = {
-        request: {:timeout=>150, :open_timeout=>150},
-        url: Lemur::ODNOKLASSNIKI_API_URL
-      }
-      @connection = init_faraday(faraday_options)
-    end
-
-    def init_faraday(faraday_options)
-      Faraday.new(faraday_options) do |faraday|
-        faraday.headers['Content-Type'] = 'application/json'
-        faraday.request  :url_encoded
-        # faraday.response :logger
-        faraday.adapter  Faraday.default_adapter
+    def check_required_options(opts)
+      required_options = [:application_secret_key, :application_key, :access_token]
+      unless required_options.all? { |it| opts.has_key? it }
+        raise "You should provide all needed options (#{required_options}) to connect odnoklassniki"
       end
     end
-
 
   end
-
 end
